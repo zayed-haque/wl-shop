a77113d5ba8f0dcaa28d5012b078f435434d92cb
zayed_haque <zayedsh2003@gmail.com>
2024-08-15 01:37:20 +0530

    Database config files added

--- wl_shop_service/__init__.py
+++ wl_shop_service/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from config import config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 ma = Marshmallow()
+migrate = Migrate()
 
 
-def create_app(config_name='default'):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
+
+    app.config.from_object('config.Config')
+
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://localhost/wl_shop_db')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     ma.init_app(app)
+    migrate.init_app(app, db)
 
     from wl_shop_service.routes.product_routes import product_bp
     app.register_blueprint(product_bp, url_prefix='/api')
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from wl_shop_service import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
