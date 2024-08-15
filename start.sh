3ef7f07268a7698b146a3f6604b291f51e6f03d6
zayed_haque <zayedsh2003@gmail.com>
2024-08-16 00:57:37 +0530

    Changed start.sh file

--- start.sh
+++ start.sh
@@ -2,15 +2,13 @@
 
 # Wait for the database to be ready
 echo "Waiting for database..."
-while ! nc -z db 5432; do
+while ! nc -z $DB_HOST $DB_PORT; do
   sleep 0.1
 done
 echo "Database is ready!"
 
 # Run database migrations
 flask db upgrade
-
-python populate_db.py
 
 # Start the Flask application
 flask run --host=0.0.0.0
